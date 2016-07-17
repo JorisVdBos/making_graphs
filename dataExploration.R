e401f60dab7f35a3c65abcf47cc534fecd5f1bf2
JorisVdBos <joris.vdbossche@gmail.com>
2016-07-17 19:06:43 +0200

    Plot 4

--- dataExploration.R
+++ dataExploration.R
@@ -1,9 +1,17 @@
-# This file checks if the data is present, then proceeds to do some exploratory analysis 
+# This file checks if the data is present, then proceeds to making the four plots
 
 # Check if the data table "householdPowerConsumption" is in the environment
 source("getData.R")
 
-# Exploration
-head(householdPowerConsumption)
-str(householdPowerConsumption)
+# Making the plots
+source("plot1.R")
+createPlot1()
 
+source("plot2.R")
+createPlot2()
+
+source("plot3.R")
+createPlot3()
+
+source("plot4.R")
+createPlot4()
--- plot4.R
+++ plot4.R
@@ -1,14 +1,32 @@
 # This code is used in the dataExploration.R code
 
 createPlot4 <- function(){
+        source("plot2.R")
+        source("plot3.R")
+        
         png(paste0("plot4.png"), width = 480, height = 480)
         par(mfrow = c(2,2))
         
-        constructPlot1()
+        constructPlot2()
+        constructPlot4()
+        constructPlot3()
+        constructPlot5()
+        
         
         dev.off()
 }
 
-constructPlot1 <- function(){
-        
+constructPlot4 <- function(){
+        with(householdPowerConsumption, plot(Date + Time, Voltage,
+                                             xlab = "datetime",
+                                             ylab = "Voltage", 
+                                             type = "n"))
+        with(householdPowerConsumption, lines(DateTime, Voltage))
+}
+
+constructPlot5 <- function(){
+        with(householdPowerConsumption, plot(Date + Time, Global_reactive_power,
+                                             xlab = "datetime",
+                                             type = "n"))
+        with(householdPowerConsumption, lines(DateTime, Global_reactive_power))
 }
